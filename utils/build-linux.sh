bcba0ed18fcd49e03710abff9a10a6b16ef73048
Qian Hong <qhong@codeweavers.com>
2014-07-16 06:07:31 +0800

    added debug output

--- utils/build-linux.sh
+++ utils/build-linux.sh
@@ -5,4 +5,6 @@ make -j4 2>&1 | grep -A2 -B2 make
 cd loader
 ln -s wine64 wine
 cd ..
-make test -k 2>&1 | grep -A2 -B2 make
+#make test -k 2>&1 | grep -A2 -B2 make
+export WINEDEBUG=+tid,+x11drv
+make test
