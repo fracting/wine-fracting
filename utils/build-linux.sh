7e89c36a566d651f34638af42bab48cb364c3348
Qian Hong <qhong@codeweavers.com>
2014-07-16 06:40:53 +0800

    run wineboot before run tests

--- utils/build-linux.sh
+++ utils/build-linux.sh
@@ -5,6 +5,5 @@ make -j4 2>&1 | grep -A2 -B2 make
 cd loader
 ln -s wine64 wine
 cd ..
-#make test -k 2>&1 | grep -A2 -B2 make
-export WINEDEBUG=+tid,+x11drv
-make test
+./wine wineboot
+make test -k 2>&1 | grep -A2 -B2 make
