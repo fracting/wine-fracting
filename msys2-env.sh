98cc7fe2215f9e14cd679773f5ec100b4f33de33
Qian Hong <qhong@codeweavers.com>
2015-03-19 09:37:20 +0800

    msys2-env.sh: Added script to initialize MSYS2 environment.

---
+++ msys2-env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env
+
+#rm -rf ~/.wine
+
+wineboot
+
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
+export LANGUAGE=en_US
+export STAGING_WRITECOPY=1
+export MSYSTEM=MSYS
+
+mkdir ~/.wine/drive_c/windows/system32/drivers/etc
+cp /etc/hosts ~/.wine/drive_c/windows/system32/drivers/etc/
+cp /etc/networks ~/.wine/drive_c/windows/system32/drivers/etc/
+cp /etc/protocols ~/.wine/drive_c/windows/system32/drivers/etc/protocol #special case
+cp /etc/services ~/.wine/drive_c/windows/system32/drivers/etc/
+
+#cd ~/.wine/drive_c/msys32/usr/bin/
