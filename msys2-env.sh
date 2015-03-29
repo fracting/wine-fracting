102e2dad7f6f0cb3e850bf8276aafeb48b750193
Qian Hong <qhong@codeweavers.com>
2015-03-30 07:41:28 +0800

    msys2-env.sh: Don't set MSYS environment variable any more, let msys2_shell.bat handle it since mintty.exe works now.

--- msys2-env.sh
+++ msys2-env.sh
@@ -8,7 +8,6 @@ export LC_ALL=en_US.utf8
 export LANG=en_US.utf8
 export LANGUAGE=en_US
 export STAGING_WRITECOPY=1
-export MSYSTEM=MSYS
 
 mkdir ~/.wine/drive_c/windows/system32/drivers/etc
 cp /etc/hosts ~/.wine/drive_c/windows/system32/drivers/etc/
